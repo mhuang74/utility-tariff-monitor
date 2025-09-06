017bbebba93e86305e7f926db0773ba7d36a62d3
Michael S. Huang <mhuang74@gmail.com>
2025-09-06 18:41:40 +0800

    update README with example of how to use extract tool to get candidate urls

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # utility-tariff-monitor
 Monitor for updates to Electric Utility Tariff PDF documents
+
+
+## Get list of tariff has changed recently from iurdb
+
+```
+cat sample_rates.json | ./extract_essential_iurdb_fields
+gzcat iurdb.json.gz | ./extract_essential_iurdb_fields > url_candidates_2.json
+```
--- resources/get_utility_rate_seed_url_candidates.sh
+++
@@ -1,3 +0,0 @@
-
-
-jq '[.[] | select(.sector == "Commercial" and .country == "USA") | {utilityName: .utilityName, eiaId: .eiaId, sector: .sector, rateName: .rateName, effectiveDate: .effectiveDate."$date", lastRevision: (.revisions | last).date."$date", sourceReference: .sourceReference}] | unique_by(.eiaId)' sample_rates.json
